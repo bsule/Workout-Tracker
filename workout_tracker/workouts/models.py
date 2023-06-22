37e878073b224113dae447e81a3b48c43ff0a718
bsule <bilal.suleiman@gmail.com>
2023-06-22 13:56:11 -0700

    created new model that stores date and reps and allows views to add to it

--- workout_tracker/workouts/models.py
+++ workout_tracker/workouts/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
+from datetime import datetime
 
 
 class Workout_Split(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, verbose_name="Routine Name:")
     id = models.AutoField(primary_key=True)
-    reps = models.TextField(null=True, blank=True)
     
     def __str__(self):
         return self.name
@@ -18,4 +18,11 @@ class Exercise(models.Model):
     def __str__(self):
         return self.name
     
+class Weight_and_reps(models.Model):
+    model = models.ForeignKey(Workout_Split, on_delete=models.CASCADE)
+    reps = models.TextField(null=True, blank=True, default="")
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.model.name
     
--- workout_tracker/workouts/views.py
+++ workout_tracker/workouts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import *
-from .models import Workout_Split
+from .models import Workout_Split, Weight_and_reps
 from django.http import HttpResponseForbidden
 
 # Create your views here.
@@ -28,29 +28,28 @@ def reps_view(request, pk):
     if my_model.user != request.user:
         return HttpResponseForbidden("You are not allowed to access this page.")
     
-    form = Weight_and_reps(request.POST)
+    if Weight_and_reps.objects.filter(model=my_model).exists():
+        weight_reps_model = Weight_and_reps.objects.filter(model=my_model).latest('date')
+    else:
+        weight_reps_model = Weight_and_reps(model=my_model)
+    
+    form = Weight_and_reps_form(request.POST)
     if request.method == 'POST':
-        
         if request.POST.get('form_submit'):
             if form.is_valid():
                 weight = form.cleaned_data['weight']
                 reps = form.cleaned_data['reps']
-                if my_model.reps != "":
-                    my_model.reps += " "
-                my_model.reps += (str(weight) + " " + str(reps))
-                my_model.save()
-                
+                if weight_reps_model.reps != "":
+                    weight_reps_model.reps += " "
+                weight_reps_model.reps += (str(weight) + " " + str(reps))
+                weight_reps_model.save()
         if request.POST.get('next_day'):
-            my_model.reps = "\n" + my_model.reps
-            my_model.save()
+            weight_reps_model = Weight_and_reps.objects.create(model=my_model)
+            
         
     
-    workoutsplit = Workout_Split.objects.get(id=pk)
+    workoutsplit = Weight_and_reps.objects.filter(model=my_model)
     context = {}
     context['form'] = form
     context['workoutsplit'] = workoutsplit
     return render(request, 'replist.html', context)
-
-# TODO create a different model with pk, weight and reps in one, and date
-# TODO then get the objects like this workoutsplit = Workout_Split.objects.get(id=pk)
-# TODO and make the table
