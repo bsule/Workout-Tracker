53a1dc9b46ef0460315bb1a74e0daa6336db8663
bsule <bilal.suleiman@gmail.com>
2023-06-22 15:52:35 -0700

    working table that shows the weight and reps

---
+++ workout_tracker/workouts/templatetags/custom_filters.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def zip_lists(list1, list2):
+    list1 = list1.split(',')
+    list2 = list2.split(',')
+    return list(zip(list1, list2))
+
