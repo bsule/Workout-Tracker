933d841479cae4c6ed7ad9920d267596a487693c
bsule <bilal.suleiman@gmail.com>
2023-06-25 15:02:05 -0700

    fixed css files not working

--- workout_tracker/workout_tracker/settings.py
+++ workout_tracker/workout_tracker/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path, os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = (BASE_DIR / 'templates')
-STATIC_DIR = (BASE_DIR /'static')
 
 
 # Quick-start development settings - unsuitable for production
