025cd091c8f8d80e573a8995bec0f2b0cb336104
bsule <bilal.suleiman@gmail.com>
2023-06-27 14:58:43 -0700

    added styling to replist

--- workout_tracker/workouts/templatetags/custom_filters.py
+++ workout_tracker/workouts/templatetags/custom_filters.py
@@ -6,5 +6,7 @@ register = template.Library()
 def zip_lists(list1, list2):
     list1 = list1.split(',')
     list2 = list2.split(',')
+    list1.pop()
+    list2.pop()
     return list(zip(list1, list2))
 
