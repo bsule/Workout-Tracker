92a184b2a5bd00bec02d6b58a501bea40870747d
bsule <bilal.suleiman@gmail.com>
2023-06-27 16:32:08 -0700

    added about page, new font, designed delete day page

--- workout_tracker/workout_tracker/urls.py
+++ workout_tracker/workout_tracker/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from .views import HomePage
+from .views import *
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
     path('registration/', include('accounts.urls'), name='registration'),
     path('', include('workouts.urls')),
+    path('about/', views.about_view, name='about_view'),
     path('admin/', admin.site.urls),
 ]
--- workout_tracker/workout_tracker/views.py
+++ workout_tracker/workout_tracker/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView
 from django.urls import reverse_lazy
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 
 class HomePage(TemplateView):
     template_name = 'index.html'
@@ -9,3 +10,7 @@ class HomePage(TemplateView):
         if not request.user.is_authenticated:
             return redirect(reverse_lazy('login'))
         return super(HomePage, self).dispatch(request, *args, **kwargs)
+
+@login_required
+def about_view(request):
+    return render(request, 'about.html')
