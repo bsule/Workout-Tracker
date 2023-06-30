a8bf3d3b41c41f7261d603a54de4edbe9b57409b
bsule <bilal.suleiman@gmail.com>
2023-06-30 00:52:50 -0700

    working graph for one workout with data from backend

--- workout_tracker/workout_tracker/views.py
+++ workout_tracker/workout_tracker/views.py
@@ -3,13 +3,16 @@ from django.urls import reverse_lazy
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from workouts.models import *
 
     
 def home_view(request):
     if not request.user.is_authenticated:
         return redirect(reverse_lazy('login'))
+    hold = Workout_Split.objects.get(id=22)
+    hold2 = Weight_and_reps.objects.filter(model=hold)
     context = {}
-    context['bench'] = 'Bench'
+    context['data'] = hold2
     return render(request, 'index.html', context)
 
 @login_required
