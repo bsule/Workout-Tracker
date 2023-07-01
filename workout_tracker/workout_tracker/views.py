00d2fb871885301ad2169f836150ee77e82cf61b
bsule <bilal.suleiman@gmail.com>
2023-06-30 23:12:27 -0700

    working graph with last 7 workouts

--- workout_tracker/workout_tracker/views.py
+++ workout_tracker/workout_tracker/views.py
@@ -9,10 +9,15 @@ from workouts.models import *
 def home_view(request):
     if not request.user.is_authenticated:
         return redirect(reverse_lazy('login'))
+    
     hold = Workout_Split.objects.get(id=22)
-    hold2 = Weight_and_reps.objects.filter(model=hold)
+    hold2 = Weight_and_reps.objects.filter(model=hold)[::-1]   # negative indexing not supported so 
+    hold2 = hold2[:7]                                          # flip the list and get the last 7 
+    hold2 = hold2[::-1]                                        # then flip back
+    
     context = {}
     context['data'] = hold2
+    context['name'] = hold.name
     return render(request, 'index.html', context)
 
 @login_required
