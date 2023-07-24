b434278a2a57591d8bb2c0b1ed383d42d86390a6
bsule <bilal.suleiman@gmail.com>
2023-07-24 14:00:25 -0700

    working on deployment

--- workout_tracker/workout_tracker/settings.py
+++ workout_tracker/workout_tracker/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-dummy-development-key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bsule.pythonanywhere.com']
 
 
 # Application definition
